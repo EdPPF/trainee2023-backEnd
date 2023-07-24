b8def8d6d00085b97056cfcbb096380da44488f7
EduardoPPF <ppfeduardo@gmail.com>
2023-07-24 15:54:55 -0300

    Model Category completa: sucesso nos testes

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,6 @@
 class Category < ApplicationRecord
+  # has_many :post_category, dependent: :destroy
+
+  validates :name, presence: :true, uniqueness: :true
+  validates :description, presence: :true, uniqueness: :true
 end
