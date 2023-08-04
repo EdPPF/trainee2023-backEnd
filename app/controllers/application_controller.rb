700f0bad6c714874451c832c3986b370a06d9c49
EduardoPPF <ppfeduardo@gmail.com>
2023-08-04 19:08:57 -0300

    Hotfix: Resolvendo problemas com Cors.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::API
+  # after_filter :cors_set_access_control_headers
+
+  # def cors_set_access_control_headers
+  #   headers['Access-Control-Allow-Origin'] = '*'
+  #   headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, PATCH'
+  #   headers['Access-Control-Request-Method'] = '*'
+  #   headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  # end
+
   private
     def admin_authentication
       unless current_user.is_admin == true
