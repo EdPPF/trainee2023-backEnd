c03ddf89a51c4e03b879d6bc894542db3a902207
EduardoPPF <ppfeduardo@gmail.com>
2023-08-05 13:35:31 -0300

    Hotfix: populei seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,17 @@ User.create(email:'edppf@email.com', name:'EdPPF', password:'abc123', is_admin:t
 User.find_by(email: 'edppf@email.com').images.attach(io: File.open('./public/github-mark.png'), filename:('github-mark.png'))
 User.find_by(email: 'edppf@email.com').images.attach(io: File.open('./public/github-mark-white.png'), filename:('github-mark-white.png'))
 
-Post.create(title:'How to 2', content:'afoaekfm', user_id:1)
-Post.find_by(title: 'How to 2').images.attach(io: File.open('./public/github-mark.png'), filename:('github-mark.png'))
-Post.find_by(title: 'How to 2').images.attach(io: File.open('./public/github-mark-white.png'), filename:('github-mark-white.png'))
+Post.create(title:'How to 2 Sekiro', content:'Git Gud', user_id:1)
+# Post.find_by(title: 'How to 2').images.attach(io: File.open('./public/github-mark.png'), filename:('github-mark.png'))
+# Post.find_by(title: 'How to 2').images.attach(io: File.open('./public/github-mark-white.png'), filename:('github-mark-white.png'))
+Post.create(title:'Shoujo Shuumatsu Ryokou e filosofia', content:'Amanhã pensamos sobre isso.', user_id:1)
+Post.create(title:'Hoje eu joguei Zelda', content:'De novo.', user_id:1)
+
+Category.create(id:1 ,name:'Daily', description:'Posts diários contando meu dia-a-dia!')
+Category.create(id:2 ,name:'Anime', description:'Discussões sobre animes e mangás!')
+Category.create(id:3 ,name:'Jogos', description:'Posts sobre jogos que tenho jogado ou quero falar sobre!')
+
+PostCategory.create(post_id: 3, category_id: 1)
+PostCategory.create(post_id: 3, category_id: 3)
+PostCategory.create(post_id: 2, category_id: 2)
+PostCategory.create(post_id: 1, category_id: 3)
